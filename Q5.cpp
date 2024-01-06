34864877c5258fbf4c835a698b27f31ac730ddd3
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2024-01-06 11:57:45 +0530

    Q5.cpp

--- Q5.cpp
+++ Q5.cpp
@@ -2,13 +2,41 @@
 using namespace std;
 int main()
 {
-    int n,i;
-    cout<<"Enter your number\n";
-    cin>>n;
-    for(i=0; n>0; i++)
+    int num;
+    cout<<"Enter odd number \n";
+    cin>>num;
+    int h=num/2;
+    for(int i=1; i<=h;i++)
     {
-      n=n/10;
+        cout<<" ";
+        for(int j=1; j<=h-i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=1; k<=(2*i)-1; k++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
     }
-    cout<<"The number of digits are "<<i;
-  return 0;
+    for(int i=1; i<=num; i++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+    for(int i=1; i<=h; i++)
+    {
+        for(int j=1; j<=i;j++)
+        {
+            cout<<" ";
+        }
+        for(int k=num-2; k>=1; k-- )
+        {
+            cout<<"*";
+            
+        }
+        cout<<endl;
+        num=num-2;
+    }
+    return 0;
 }
