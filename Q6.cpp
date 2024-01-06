3130b58242f84bd85cd9e7ab159457101b2b055e
Isha-101 <145088595+Isha-101@users.noreply.github.com>
2024-01-06 11:58:51 +0530

    Q6.cpp

--- Q6.cpp
+++ Q6.cpp
@@ -2,14 +2,41 @@
 using namespace std;
 int main()
 {
-     int n,i,r;
-    cout<<"Enter your number\n";
-    cin>>n;
-    for(i=0; n>0; i++)
+    int num;
+    cout<<"Enter an odd number \n";
+    cin>>num;
+    int h=num/2;
+    for(int i=0; i<=h;i++)
     {
-      r=n%10;
-      cout<<r;
-      n=n/10;
-    }   
-  return 0;
+        for(int j=1; j<=h+1-i;j++)
+        {
+            cout<<"*";
+        }
+        for(int j=1; j<=(2*i)+1;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=h+1-i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=1; i<=h; i++)
+    {
+        for(int j=1; j<=i+1; j++)
+        {
+            cout<<"*";
+        }
+        for(int j=1; j<= 2*(h-i)+1;j++)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=i+1; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
